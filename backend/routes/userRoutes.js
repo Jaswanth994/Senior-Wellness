94181e38fb54d2f888928aed1ea70136c8da9f3f
G_Jaswanth <jaswanthgosu0909@gmail.com>
2024-11-09 23:23:15 +0530

    small mistake debugged

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/Users');
 
 // Add or update user results
 router.post('/add-result', async (req, res) => {
