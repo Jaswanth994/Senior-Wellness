d14a0579a11189da070a2f00ddf1d39d914fe265
G_Jaswanth <jaswanthgosu0909@gmail.com>
2024-11-11 15:38:48 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,44 +14,17 @@ import Home from './components/Home.jsx';
 import Quiz from './components/Quiz.jsx';
 import Results from './components/Results.jsx';
 import ArticlesPage from './components/ArticlesPage.jsx';
-// import { useTranslation } from 'react-i18next';
-// import LanguageSwitcher from './LanguageSwitcher';
-import TranslationComponent from './components/TranslationComponent';
 
 
 function App() {
-  // const { t } = useTranslation();  // Initialize the translation function
-  useEffect(() => {
-    const addScript = () => {
-      const script = document.createElement('script');
-      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-      script.async = true;
-      document.body.appendChild(script);
-    };
-
-    // Google Translate widget initialization function
-    window.googleTranslateElementInit = () => {
-      new window.google.translate.TranslateElement(
-        {
-          pageLanguage: 'en',
-          includedLanguages: 'hi,te,ta,kn,ml,gu,bn,mr,pa,en',
-          layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
-        },
-        'google_translate_element'
-      );
-    };
-
-    addScript();
-  }, []);
 
   const textToTranslate = 'Hello, welcome to our website!';
 
   return (
     <div className="App">
        
-    {/* Add Navigation component if needed */}
+   
     {/* <Navigation /> */}
-    {/* <div id="google_translate_element" className="w-full mb-4"></div> */}
       {/* Define the routing logic */}
       <Routes>
         {/* Default route to redirect to the Homepage */}
@@ -72,14 +45,7 @@ function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
 
-       {/* Welcome popup */}
-       {/* <WelcomePopup /> */}
-
-{/* Language Switcher for changing languages */}
-{/* <LanguageSwitcher /> */}
-
-<h1>Language Translation App</h1>
-<TranslationComponent textToTranslate={textToTranslate} />
+      
 
     </div>
   );
